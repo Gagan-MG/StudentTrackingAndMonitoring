d9edacab28593e6cce22ca372a1afeff4dd703c8
Gaganmgithub <mayagagan123@gmail.com>
2024-04-06 23:51:27 +0530

    modified index.js

--- StudentTraking/src/Components/Login.jsx
+++ StudentTraking/src/Components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    axios.post('https://localhost:3000/auth/adminlogin')
+    axios.post('http://localhost:3000/auth/adminlogin')
     .then(result => console.log(result))
     .catch(err => console.log(err))
   }
@@ -23,18 +23,18 @@ const Login = () => {
             <form onSubmit={handleSubmit}>
                 <div className='mb-3'>
                     <label htmlFor='email'> <strong>Email:</strong></label>
-                    <input type='email' name='email' autoComplete='off' placeholder='enter email' 
-                      onChange={() => setValues({...values, email : e.target.value})} className='form-control rounded-0'></input>
+                    <input type='email' name='email' autoComplete='off' placeholder='Enter email' 
+                      onChange={(e) => setValues({...values, email : e.target.value})} className='form-control rounded-0'></input>
                 </div>
                 <div className='mb-3'>
                     <label htmlFor='password'> <strong>Passwrod:</strong></label>
-                    <input type='password' name='password' placeholder='enter password' 
-                    onChange={() => setValues({...values, password : e.target.value})} className='form-control rounded-0'></input>
+                    <input type='password' name='password' placeholder='Enter password' 
+                    onChange={(e) => setValues({...values, password : e.target.value})} className='form-control rounded-0'></input>
                 </div>
                 <button className='btn btn-success w-100 rounded-0 mb-2'>Log In</button>
                 <div className='mb-1'>
                     <input type='checkbox' name='tick' id='tick' className='me-2'></input>
-                    <label htmlFor='password'> You are agree to our Terms and Condition</label>
+                    <label htmlFor='password'>You are agree to our Terms and Condition</label>
                 </div>
             </form>
         </div>
