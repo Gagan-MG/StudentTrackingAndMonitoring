0545d70fc1425789b30e00c468f9f7f328edf995
Gaganmgithub <mayagagan123@gmail.com>
2024-04-10 00:23:21 +0530

    added Home Page

--- StudentTraking/src/Components/Home.jsx
+++ StudentTraking/src/Components/Home.jsx
@@ -1,8 +1,120 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [adminTotal, setAdminTotal] = useState(0)
+  const [studentTotal, setStudentTotal] = useState(0)
+  const [ageTotal, setAgeTotal] = useState(0)
+  const [admins, setAdmins] = useState([])
+
+  useEffect(() => {
+    adminCount();
+    studentCount();
+    ageCount();
+    AdminRecords();
+  }, [])
+
+  const AdminRecords = () => {
+    axios.get('http://localhost:3000/auth/admin_records')
+    .then(result => {
+      if(result.data.Status) {
+        setAdmins(result.data.Result)
+      } else {
+         alert(result.data.Error)
+      }
+    })
+  }
+  const adminCount = () => {
+    axios.get('http://localhost:3000/auth/admin_count')
+    .then(result => {
+      if(result.data.Status) {
+        setAdminTotal(result.data.Result[0].admin)
+      }
+    })
+  }
+  const studentCount = () => {
+    axios.get('http://localhost:3000/auth/student_count')
+    .then(result => {
+      if(result.data.Status) {
+        setStudentTotal(result.data.Result[0].student)
+      }
+    })
+  }
+  const ageCount = () => {
+    axios.get('http://localhost:3000/auth/age_count')
+    .then(result => {
+      if(result.data.Status) {
+        setAgeTotal(result.data.Result[0].ageOFStu)
+      } else {
+        alert(result.data.Error)
+      }
+    })
+  }
   return (
-    <div>Home</div>
+    <div>
+      <div className='p-3 d-flex justify-content-around mt-3'>
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+          <div className='text-center pb-1'>
+            <h4>Admin</h4>
+          </div>
+          <hr />
+          <div className='d-flex justify-content-between'>
+            <h5>Total:</h5>
+            <h5>{adminTotal}</h5>
+          </div>
+        </div>
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+          <div className='text-center pb-1'>
+            <h4>Student</h4>
+          </div>
+          <hr />
+          <div className='d-flex justify-content-between'>
+            <h5>Total:</h5>
+            <h5>{studentTotal}</h5>
+          </div>
+        </div>
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+          <div className='text-center pb-1'>
+            <h4>Age</h4>
+          </div>
+          <hr />
+          <div className='d-flex justify-content-between'>
+            <h5>Total:</h5>
+            <h5>{ageTotal}</h5>
+          </div>
+        </div>
+      </div>
+      <div className='mt-4 px-5 pt-3'>
+        <h3>List of Admins</h3>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              admins.map(a => (
+                <tr>
+                  <td>{a.email}</td>
+                  <td>
+                  <button
+                    className="btn btn-info btn-sm me-2">
+                    Edit
+                  </button>
+                  <button
+                    className="btn btn-warning btn-sm" >
+                    Delete
+                  </button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
--- StudentTraking/src/Components/Student.jsx
+++ StudentTraking/src/Components/Student.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import axios from 'axios'
 
 const Student = () => {
   const [student, setStudent] = useState([])
+  const navigate = useNavigate()
   useEffect(() => {
     axios.get('http://localhost:3000/auth/student')
       .then(result => {
@@ -14,6 +15,16 @@ const Student = () => {
         }
       }).catch(err => console.log(err))
   }, [])
+  const handleDelete =(id) => {
+    axios.delete('http://localhost:3000/auth/delete_student/'+id)
+    .then(result => {
+      if(result.data.Status) {
+        window.location.reload()
+      } else {
+        alert(result.data.Error)
+      }
+    })
+  }
   return (
     <div className='px-5 mt-3'>
       <div className='d-flex justify-content-center'>
@@ -39,17 +50,17 @@ const Student = () => {
                 <tr>
                   <td>{e.name}</td>
                   <td>
-                    <img 
-                      src={`http://localhost:3000/Images/` + e.image} 
-                      className='student_image' 
+                    <img
+                      src={`http://localhost:3000/Images/` + e.image}
+                      className='student_image'
                     />
                   </td>
                   <td>{e.email}</td>
                   <td>{e.address}</td>
                   <td>{e.age}</td>
                   <td>
-                    <Link to={`/dashboard/edit_student/`+ e.id} className='btn btn-info btn-sm me-2'>Edit</Link>
-                    <button className='btn btn-warning btn-sm'>Delete</button>
+                    <Link to={`/dashboard/edit_student/` + e.id} className='btn btn-info btn-sm me-2'>Edit</Link>
+                    <button className='btn btn-warning btn-sm' onClick={() => handleDelete(e.id)}>Delete</button>
                   </td>
                 </tr>
               ))
